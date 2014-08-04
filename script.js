72d9ee17c9781424a781499b599ae1e807e9abbb
David Josefson <david.josefson@gmail.com>
2014-08-04 13:45:08 +0200

    Adds interval to add a new snake head every 500ms and changes the currentY/Xpos accordingly. Can change direction as well.

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 /*global $:false */
 
 var currentDirection = "right";
+var startXpos = 50;
+var startYpos = 50;
+var currentXpos = 50;
+var currentYpos = 50;
 
 $(document).ready(function(){
     
@@ -13,31 +17,54 @@ $(document).ready(function(){
         switch(key.which) {
             //LEFT
             case 37: 
-                $('#snake').animate({left: '-=10px'}, 50);
+                currentDirection = "left";
                 break;
             
             //UP
             case 38:
-                $('#snake').animate({top: '-=10px'}, 50);
+                currentDirection = "up";
                 break;                
             
             //RIGHT
             case 39: 
-                $('#snake').animate({left: '+=10px'}, 50);
+                currentDirection = "right";
                 break;
             //DOWN
             case 40:
-                $('#snake').animate({top: '+=10px'}, 50);
+                //$('#snake').animate({top: '+=10px'}, 50);
+                currentDirection = "down";
                 break;
         
         }
-    });    
+    });
     
 });
 
 function moveSnake() {
     var myInterval = setInterval(function(){
-        $('#snake').animate({left: '+=10px'}, 50);
+        drawSnakeHead(currentXpos,currentYpos);
+        
+        switch(currentDirection){
+            case "left":
+                currentXpos -= 10;
+                break;
+            case "up":
+                currentYpos -= 10;
+                break;
+            case "right":
+                currentXpos += 10;
+                break;
+            case "down":
+                currentYpos += 10;
+                break;
+        }
+
     }, 500);
+    
+}
+
+function drawSnakeHead(xPos, yPos) {
+    $('#board').append('<div class="snakeHead" style="left: ' + xPos + 'px; top:' + yPos + 'px;"></div>');
+    //$('#board .snakeHead').css('top', yPos).css('left', xPos);
 }
 
