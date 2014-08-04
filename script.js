d4f766d8614851577da52f2d6933a35161bf9b69
David Josefson <david.josefson@gmail.com>
2014-08-04 16:56:44 +0200

    Moved direction-switch from moveSnake-function to drawSnakeHead-function
    ..which fixed the direction switching issues. It's smoother now.

--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 
 //TODO: [x]När man ändrar direction så ska den ta bort den nya intervallen och skapa en ny
 //TODO: [x]Om man klickar på en knapp i "samma" riktning ska den ignorera detta (så att man inte kan speeda upp snaken)
+//TODO: 
 
 var currentDirection = "right";
-var startXpos = 50;
-var startYpos = 50;
 var currentXpos = 50;
 var currentYpos = 50;
 var myInterval;
+var pixelsToMove = 5;
+var snakeSpeed = 50;
 
 $(document).ready(function(){
     
@@ -53,29 +54,29 @@ function moveSnake(direction) {
     currentDirection = direction;
     
     myInterval = setInterval(function(){
-        drawSnakeHead(currentXpos,currentYpos);
-        
-        switch(currentDirection){
-            case "left":
-                currentXpos -= 10;
-                break;
-            case "up":
-                currentYpos -= 10;
-                break;
-            case "right":
-                currentXpos += 10;
-                break;
-            case "down":
-                currentYpos += 10;
-                break;
-        }
+        drawSnakeHead(direction);
 
-    }, 200);
+    }, 50);
     
 }
 
-function drawSnakeHead(xPos, yPos) {
-    $('#board').append('<div class="snakeHead" style="left: ' + xPos + 'px; top:' + yPos + 'px;"></div>');
+function drawSnakeHead(direction) {
+    switch(direction){
+        case "left":
+            currentXpos -= pixelsToMove;
+            break;
+        case "up":
+            currentYpos -= pixelsToMove;
+            break;
+        case "right":
+            currentXpos += pixelsToMove;
+            break;
+        case "down":
+            currentYpos += pixelsToMove;
+            break;
+    }
+    
+    $('#board').append('<div class="snakeHead" style="left: ' + currentXpos + 'px; top:' + currentYpos + 'px;"></div>');
     //$('#board .snakeHead').css('top', yPos).css('left', xPos);
 }
 
