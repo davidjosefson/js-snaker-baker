b8401d96cf120abc52353740e7cce4debc1c7b08
David Josefson <david.josefson@gmail.com>
2014-08-04 22:43:49 +0200

    Surrounded all code in a function to add closure - which removes global vars

--- script.js
+++ script.js
@@ -7,82 +7,83 @@
 
 /*jslint browser: true, devel: true, plusplus: true, todo: true, jQuery:false */
 /*global $:false */
+(function() {
+    var currentDirection = "right";
+    var currentXpos = 50;
+    var currentYpos = 50;
+    var myInterval;
+    var pixelsToMove = 5;
+    var snakeSpeed = 50;
 
-var currentDirection = "right";
-var currentXpos = 50;
-var currentYpos = 50;
-var myInterval;
-var pixelsToMove = 5;
-var snakeSpeed = 50;
+    $(document).ready(function(){
 
-$(document).ready(function(){
-    
-    moveSnake("right");
-    
-    $(document).keydown(function(key){
-        //$('#board').html('This key is pressed: ' + key.which);    
+        moveSnake("right");
 
-        switch(key.which) {
-            //LEFT
-            case 37: 
-                if((currentDirection != "right") && (currentDirection != "left"))
-                    moveSnake("left");
+        $(document).keydown(function(key){
+            //$('#board').html('This key is pressed: ' + key.which);    
+
+            switch(key.which) {
+                //LEFT
+                case 37: 
+                    if((currentDirection != "right") && (currentDirection != "left"))
+                        moveSnake("left");
+                    break;
+
+                //UP
+                case 38:
+                    if((currentDirection != "down") && (currentDirection != "up"))
+                        moveSnake("up");
+                    break;                
+
+                //RIGHT
+                case 39: 
+                    if((currentDirection != "right") && (currentDirection != "left"))
+                        moveSnake("right");
+                    break;
+                //DOWN
+                case 40:
+                    if((currentDirection != "down") && (currentDirection != "up"))
+                        moveSnake("down");
+                    break;
+                //SPACE
+                case 32:
+                    clearInterval(myInterval);
+                    break;
+            }
+        });
+
+    });
+
+    function moveSnake(direction) {
+        clearInterval(myInterval);
+
+        currentDirection = direction;
+
+        myInterval = setInterval(function(){
+            drawSnakeHead(direction);
+
+        }, 50);
+
+    }
+
+    function drawSnakeHead(direction) {
+        switch(direction){
+            case "left":
+                currentXpos -= pixelsToMove;
                 break;
-            
-            //UP
-            case 38:
-                if((currentDirection != "down") && (currentDirection != "up"))
-                    moveSnake("up");
-                break;                
-            
-            //RIGHT
-            case 39: 
-                if((currentDirection != "right") && (currentDirection != "left"))
-                    moveSnake("right");
+            case "up":
+                currentYpos -= pixelsToMove;
                 break;
-            //DOWN
-            case 40:
-                if((currentDirection != "down") && (currentDirection != "up"))
-                    moveSnake("down");
+            case "right":
+                currentXpos += pixelsToMove;
                 break;
-            //SPACE
-            case 32:
-                clearInterval(myInterval);
+            case "down":
+                currentYpos += pixelsToMove;
                 break;
         }
-    });
-    
-});
 
-function moveSnake(direction) {
-    clearInterval(myInterval);
-    
-    currentDirection = direction;
-    
-    myInterval = setInterval(function(){
-        drawSnakeHead(direction);
-
-    }, 50);
-    
-}
-
-function drawSnakeHead(direction) {
-    switch(direction){
-        case "left":
-            currentXpos -= pixelsToMove;
-            break;
-        case "up":
-            currentYpos -= pixelsToMove;
-            break;
-        case "right":
-            currentXpos += pixelsToMove;
-            break;
-        case "down":
-            currentYpos += pixelsToMove;
-            break;
+        $('#board').append('<div class="snakeHead" style="left: ' + currentXpos + 'px; top:' + currentYpos + 'px;"></div>');
+        //$('#board .snakeHead').css('top', yPos).css('left', xPos);
     }
-    
-    $('#board').append('<div class="snakeHead" style="left: ' + currentXpos + 'px; top:' + currentYpos + 'px;"></div>');
-    //$('#board .snakeHead').css('top', yPos).css('left', xPos);
-}
+})();
 
