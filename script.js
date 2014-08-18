92b80e5f51358d763042fb63d5899c7338b7905f
David Josefson <david.josefson@gmail.com>
2014-08-18 15:47:43 +0200

    Added functions to check if the snake is colliding with its own tail
    If it is colliding the game is over.

--- script.js
+++ script.js
@@ -157,12 +157,28 @@
                 break;
         }
         
-        //Draws the head on the board
-        $('#board').append('<div class="snakeHead" style="left: ' + currentXpos + 'px; top:' + currentYpos + 'px;"></div>');
-        
-        //Adds coordinates to the array
-        addHeadToArray(currentXpos, currentYpos);
-        
+        //Check if the coordinates already exists (if the snake has collided with itself)
+        if(checkTailCollision(currentXpos, currentYpos)) {
+            gameOver();
+        }
+        else {
+             //Draws the head on the board
+            $('#board').append('<div class="snakeHead" style="left: ' + currentXpos + 'px; top:' + currentYpos + 'px;"></div>');
+
+            //Adds coordinates to the array
+            addHeadToArray(currentXpos, currentYpos);    
+        }
+    }
+    
+    function checkTailCollision(currentXpos, currentYpos) {
+        if(snakeBodyArray.length > 0) {
+            for(var i = 0; i < snakeBodyArray.length; i++) {
+                if((snakeBodyArray[i].xPos == currentXpos) && (snakeBodyArray[i].yPos == currentYpos)){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
     
     function removeSnakeTail() {
